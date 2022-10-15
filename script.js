795e00402b91e225180b89252552688e9ec39577
usama-365 <usama-365@protonmail.com>
2022-10-15 12:58:39 +0500

    Created revealing section effect

--- script.js
+++ script.js
@@ -102,3 +102,21 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${nav.getBoundingClientRect().height}px`,
 });
 headerObserver.observe(header);
+
+// Reveal section
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  section.classList.add('section--hidden');
+  sectionObserver.observe(section);
+});
