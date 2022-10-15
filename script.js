f0eee326e9565160da62893c17baf86d212ddbdb
usama-365 <usama-365@protonmail.com>
2022-10-15 14:00:44 +0500

    Added slider

--- script.js
+++ script.js
@@ -138,3 +138,64 @@ const imgObserver = new IntersectionObserver(loadImg, {
   rootMargin: '200px',
 });
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Sliders
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const maxSlides = slides.length;
+  const dotContainer = document.querySelector('.dots');
+
+  const createDots = function () {
+    slides.forEach((_, i) => {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+  createDots();
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+    document
+      .querySelector(`.dots__dot[data-slide='${slide}']`)
+      .classList.add('dots__dot--active');
+  };
+  const goToSlide = function (slide) {
+    if (slide >= 0 && slide < maxSlides) {
+      slides.forEach(
+        (s, i) => (s.style.transform = `translateX(${(i - slide) * 100}%)`)
+      );
+      activateDot(slide);
+    }
+  };
+  let currentSlide = 0;
+  goToSlide(currentSlide);
+  const goRight = function () {
+    ++currentSlide;
+    goToSlide(currentSlide);
+  };
+  const goLeft = function () {
+    --currentSlide;
+    goToSlide(currentSlide);
+  };
+  btnRight.addEventListener('click', goRight);
+  btnLeft.addEventListener('click', goLeft);
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') goLeft();
+    else if (e.key === 'ArrowRight') goRight();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+    }
+  });
+};
+
+slider();
